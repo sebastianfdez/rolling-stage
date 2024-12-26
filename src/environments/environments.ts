428499390fbb0d54631cfbbdb67342195fc06144
sebastianfdeztagle <sebastianfdeztagle@gmail.com>
2024-12-26 02:19:13 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyAo7M1OyumVZZgyAZTuEB2n7OF8eF3nmO8',
+      authDomain: 'rolling-stage.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'rolling-stage',
+      storageBucket: 'rolling-stage.firebasestorage.app',
+      messagingSenderId: '990740787076',
+      appId: '1:990740787076:web:3d163192407be55a64f7d1',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyDeiZWzgK1jc-5K9zOwaBKxsCb8cd7rOK0',
+  debug_token: ''
+};
